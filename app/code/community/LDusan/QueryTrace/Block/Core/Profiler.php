d3086dd88fefe9860c79a928ca73ad470a1c5aa0
Dusan Lukic <ldusan84@gmail.com>
2017-01-03 11:26:01 +0100

    Small logic fix

--- app/code/community/LDusan/QueryTrace/Block/Core/Profiler.php
+++ app/code/community/LDusan/QueryTrace/Block/Core/Profiler.php
@@ -5,9 +5,9 @@ class LDusan_QueryTrace_Block_Core_Profiler extends Mage_Core_Block_Profiler
     protected function _toHtml()
     {
         if (Mage::helper('ldusan_querytrace')->isEnabled()) {
-            return '';    
+            return parent::_toHtml();
         }
-        return parent::_toHtml();
+        return '';
     }
 
 }
